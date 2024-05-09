e4acc5f132bc924c5891d4078648d109063c26ec
vessoleck17 <157706359+vessoleck17@users.noreply.github.com>
2024-05-08 21:21:49 -0300

    Update sexo.js

--- model/DAO/sexo.js
+++ model/DAO/sexo.js
@@ -22,15 +22,17 @@ const selectAllSexo = async function(){
     }
 }
 
-const selectSexoById = async function(){
+const selectSexoById = async function(id){
     try{
+
+       
         let sql = `select * from tbl_sexo where id = ${id}`
 
         let rsSexo = await prisma.$queryRawUnsafe(sql)
 
         return rsSexo
     }catch(error){
-        return false
+        return error
     }
 }
 
